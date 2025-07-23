d51df53e61952e1fbfe7b7412954ab6a3f85ee6c
Naveen Kumar Bonkuri <naveenkumar.bonkurii@gmail.com>
2025-07-23 22:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (96.70%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>();
+
+    public int fib(int n) {
+        if (n <= 1) return n;
+        if (memo.containsKey(n)) return memo.get(n);
+
+        int result = fib(n - 1) + fib(n - 2);
+        memo.put(n, result);
+        return result;
+    }
+}
