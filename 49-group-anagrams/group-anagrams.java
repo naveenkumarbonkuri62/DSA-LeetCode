d3201331dc3833f2bcf99da460100951cfff0aec
Naveen Kumar Bonkuri <naveenkumar.bonkurii@gmail.com>
2025-11-10 22:41:54 +0530

    Time: 7 ms (63.30%) | Memory: 49.6 MB (9.65%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,34 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs==null || strs.length==0) return new ArrayList<>();
-        Map<String,List<String>> map = new HashMap<>();
-       
-        for(String str:strs){
-            String freqStr = countFreq(str);
-            if(map.containsKey(freqStr)){
-                map.get(freqStr).add(str);
-            }else{
-                List<String> list = new ArrayList<>();
-                list.add(str);
-                map.put(freqStr,list);
+        Map<String,List<String>> anaGroups = new HashMap<>();
 
+        for(String str:strs){
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key  = new String(charArray);
+            if(!anaGroups.containsKey(key)){
+                anaGroups.put(key,new ArrayList<>(List.of(str)));
+            }
+            else{
+                anaGroups.get(key).add(str);
             }
         }
-        return new ArrayList<>(map.values());  
+        return new ArrayList<>(anaGroups.values());
+        
     }
-    private static String countFreq(String str) {
-			if(str.length()==0) return "";
-			int[] freq = new int[26];
-			for(char ch :str.toCharArray()) {
-				freq[ch-'a']++;
-			}
-			StringBuilder sb = new StringBuilder("");
-			char ch = 'a';
-			for(int i: freq) {
-				sb.append(ch);
-				sb.append(i);
-				ch++;
-			}
-			return sb.toString();
-		}
 }
