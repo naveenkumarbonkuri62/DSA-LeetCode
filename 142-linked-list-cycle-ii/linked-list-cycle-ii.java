2578cdbc7dcd6f3d7bc9d6fccc90af80cb4dbd5a
Naveen Kumar Bonkuri <naveenkumar.bonkurii@gmail.com>
2025-11-16 21:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (8.35%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -30,3 +30,22 @@ public class Solution {
         return head; // Pr return p1, they both will return the tal where cycle starts..
     }
 }
+
+
+/** (Using Hashset) **/
+// Runtime: 3 ms, faster than 81.55% of Java online submissions for Linked List Cycle II.
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         // Take a HashSet to store unique values and we are storing address of ListNodes which should be unique if there is no cycle.
+//         HashSet<ListNode> set = new HashSet<>();
+//         // Traverse elements of the list through the loop...
+//         // Insert current node inside the set and move forward.
+//         while (head != null && set.add(head)) {
+//             head = head.next;
+//         }
+//         // If node already present inside the set, It means we reach that node again then return that node.
+//         return head;
+//     }
+// }
